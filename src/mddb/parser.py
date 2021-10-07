e9fbef58fd30c991c6a5270e4121ed173a33e059
Zeebrow <mzborowski@yahoo.com>
2021-10-06 23:58:38 -0400

    roast candidate

--- src/mddb/parser.py
+++ src/mddb/parser.py
@@ -1,38 +1,9 @@
 #avert eyes
 from mdtypes import *
+from mdregex import *
 #revert eyes
 import re
 # https://spec.commonmark.org/0.30/#appendix-a-parsing-strategy
-
-class Regex:
-    def __init__(self):
-        self.ATX_HEADING = '^\s{0,3}(?P<level>[#]{1,6})(?:\s*)?(?P<heading>.*)'
-        self.SETEXT = '^[ ]{0,3}(?P<setext>[=\-])(?P=setext)*\s*'
-        self.CODEFENCE = '^[ ]{0,3}(?P<level>[`~]{3,})(?P<lang>[\w]*)'
-        self.THEM_BRK = '^[ ]{0,3}(?P<ch>[\-\_\*])(?:(?P=ch)|\s*){2,}\Z'
-    regex = {
-            # groups: level, text
-            'pound_heading': '^\s{0,3}(?P<level>[#]{1,6})(?:\s*)?(?P<heading>.*)',
-            
-            # NOTE: whatever string precedes without line break will render
-            # so basically, an entire paragraph can be a heading.
-            # https://spec.commonmark.org/0.30/#setext-heading
-            #
-            # groups: setext
-            'setext': '^[ ]{0,3}(?P<setext>[=\-])(?P=setext)*\s*', 
-
-            # https://spec.commonmark.org/0.30/#code-fence
-            #
-            # groups: lang
-            'codefence': '^[ ]{0,3}(?P<level>[`~]{3,})(?P<lang>[\w]*)',
-
-            # NOTE: will also match setext
-            # https://spec.commonmark.org/0.30/#thematic-break
-            #
-            # groups: ch(arcter used)
-            'thematic_break': '^[ ]{0,3}(?P<ch>[\-\_\*])(?:(?P=ch)|\s*){2,}\Z' 
-            }
-
 
 class Pass1:
     pass
